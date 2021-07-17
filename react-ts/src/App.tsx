cb21c99b0632f377d06706a8bc7a5da1a4ab6139
kscheltema <k87scheltema@outlook.com>
2021-07-17 09:50:23 +0200

    clear TS structure for clean code construction

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Todos from './components/Todos';
+import Todo from './models/todo';
 
 function App() {
+  const todos = [
+new Todo('Learn React'),
+new Todo('Learn TypeScript')
+  ];
+
   return (
     <div className="App">
-<Todos items={['Learn React', 'Learn TypeScript']}/> 
+<Todos items={todos}/> 
 {/* the error is because props is expected  */}
     </div>
   );
--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import Todo from '../models/todo';
 
-const Todos: React.FC<{items: string[]}> = (props) => { 
+const Todos: React.FC<{items: Todo[]}> = (props) => { 
   //functional component: is a function that acts as a functional component 
   //<{}> here define our own props and merge the existing Object type with the new
   return(
     <ul>
-{props.items.map((item) => <li key={item}>{item}</li>)}
+{props.items.map((item) => <li key={item.id}>{item.text}</li>)}
     </ul>
   );
 }
