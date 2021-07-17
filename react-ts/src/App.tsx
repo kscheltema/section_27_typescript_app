9579c82c113d6fc22708383697a67e60d4079484
kscheltema <k87scheltema@outlook.com>
2021-07-17 14:39:18 +0200

    reusing a TS function in App

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
@@ -8,8 +9,13 @@ new Todo('Learn React'),
 new Todo('Learn TypeScript')
   ];
 
+  const addTodoHandler = (todoText: string) => {
+
+  }
+
   return (
     <div className="App">
+      <NewTodo onAddTodo={addTodoHandler}/>
 <Todos items={todos}/> 
 {/* the error is because props is expected  */}
     </div>
