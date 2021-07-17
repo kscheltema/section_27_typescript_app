04fecb8db19c1dcce0abce72ce1214e47387582b
kscheltema <k87scheltema@outlook.com>
2021-07-17 10:17:46 +0200

    using an Component as a return with TS

--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Todo from '../models/todo';
+import TodoItem from './TodoItem';
 
 const Todos: React.FC<{items: Todo[]}> = (props) => { 
   //functional component: is a function that acts as a functional component 
   //<{}> here define our own props and merge the existing Object type with the new
   return(
     <ul>
-{props.items.map((item) => <li key={item.id}>{item.text}</li>)}
+{props.items.map((item) => <TodoItem key={item.id} text={item.text}/>)}
     </ul>
   );
 }
