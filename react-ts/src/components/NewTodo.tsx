f84c4bc74835f5c05cf7f32146329fa8dfba5d88
kscheltema <k87scheltema@outlook.com>
2021-07-17 15:20:46 +0200

    css imports

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,4 +1,5 @@
 import React, {useRef} from "react";
+import classes from './NewTodo.module.css'
 
 const NewTodo: React.FC<{onAddTodo: (text: string) => void}> = (props) => { 
   //{onAddTodo:(parameters) => define the return- TS way of defining a function
@@ -19,7 +20,7 @@ event.preventDefault();
   };
   
   return (
-    <form onSubmit={submitHandler}>
+    <form className={classes.form} onSubmit={submitHandler}>
 <label htmlFor="text">Todo text</label>
 <input type="text" id="text" ref={todoTextInputRef}></input>
 <button>Add Todo</button>
--- react-ts/src/components/Todos.tsx
+++ react-ts/src/components/Todos.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Todo from '../models/todo';
 import TodoItem from './TodoItem';
+import classes from './Todos.module.css'
 
 const Todos: React.FC<{items: Todo[]}> = (props) => { 
   //functional component: is a function that acts as a functional component 
   //<{}> here define our own props and merge the existing Object type with the new
   return(
-    <ul>
+    <ul className={classes.todos}>
 {props.items.map((item) => <TodoItem key={item.id} text={item.text}/>)}
     </ul>
   );
